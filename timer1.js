80a13351a8ba0320cd98b3b141f041ebda15fb61
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-22 17:06:09 -0400

    Update timer1.js
    Refactored function

--- timer1.js
+++ timer1.js
@@ -1,8 +1,15 @@
+/*
+Implement an alarm clock / timer which will beep after a specified amount of time has passed. 
+The user can specify an unlimited number of alarms using command line arguments
+*/
+
 const timer = process.argv.slice(2);
   if (timer !== []) {
+    timer.sort(function(a, b){return a - b});
     for (let time of timer) {
-      if (isNaN(time) === false && time >= 0) {
-        timer.sort(function(a, b){return a - b});
+      console.log(time);
+      console.log(timer);
+      if (isNaN(time) === false && time >= 0) {  
         setTimeout(() => {
           process.stdout.write('\x07');
         }, time * 1000);
